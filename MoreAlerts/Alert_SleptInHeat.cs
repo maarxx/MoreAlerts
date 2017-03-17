4d50f562261e4398c3976d43ca3784b38409b995
maarxx <chris.lewark@gmail.com>
2017-03-16 23:54:49 -0400

    Initial test

---
+++ MoreAlerts/Alert_SleptInHeat.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    public class Alert_SleptInHeat : Alert_Thought
+    {
+
+        public Alert_SleptInHeat()
+        {
+            defaultLabel = "Uncomfortably Hot";
+            defaultExplanation = "Colonist is uncomfortably hot.";
+        }
+
+        protected override ThoughtDef Thought => ThoughtDefOf.SleptInHeat;
+
+    }
+}
