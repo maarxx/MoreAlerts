3af3727e0f11a2dbc56c08f92720d05fa5ece52a
maarxx <chris.lewark@gmail.com>
2017-03-17 15:46:19 -0400

    Added Alert_PawnRestricted

---
+++ MoreAlerts/Alert_PawnRestricted.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_PawnRestricted : Alert
+    {
+
+        List<Pawn> restrictedPawns;
+
+        int lastTick;
+
+        public Alert_PawnRestricted()
+        {
+            this.defaultLabel = "X restricted pawns";
+            this.defaultExplanation = "Some pawns are restricted!";
+            this.restrictedPawns = new List<Pawn>();
+            this.lastTick = 0;
+        }
+
+        public override AlertReport GetReport()
+        {
+            GetRestrictedPawns();
+            return this.restrictedPawns.FirstOrDefault();
+        }
+
+        public override string GetLabel()
+        {
+            GetRestrictedPawns();
+            return "" + restrictedPawns.Count() + " restricted pawns";
+        }
+
+        public override string GetExplanation()
+        {
+            GetRestrictedPawns();
+            return base.GetExplanation();
+        }
+
+        private void GetRestrictedPawns()
+        {
+
+            int curTick = Find.TickManager.TicksGame;
+            if (lastTick + 10 > curTick)
+            {
+                return;
+            }
+            else
+            {
+                restrictedPawns = new List<Pawn>();
+                foreach (Pawn p in PawnsFinder.AllMaps_FreeColonistsSpawned)
+                {
+                    Area_Allowed area = (Area_Allowed) p.playerSettings.AreaRestriction;
+                    if (area != null && area.Label != "Psyche" && area.Label != "ToxicH")
+                    {
+                        restrictedPawns.Add(p);
+                    }
+                }
+                lastTick = curTick;
+            }
+
+        }
+    }
+}
