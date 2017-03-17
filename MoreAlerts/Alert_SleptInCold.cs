33dd420bd0c4392c4659b1c5b2279f5461630c92
maarxx <chris.lewark@gmail.com>
2017-03-17 14:31:28 -0400

    Added Slept in Cold

---
+++ MoreAlerts/Alert_SleptInCold.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    public class Alert_SleptInCold : Alert_Thought
+    {
+
+        public Alert_SleptInCold()
+        {
+            defaultLabel = "Slept in Cold";
+            defaultExplanation = "Colonist slept in Cold.";
+        }
+
+        protected override ThoughtDef Thought => ThoughtDefOf.SleptInCold;
+
+    }
+}
