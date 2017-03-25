7df541ce21531990a32cb26db57e1139eab90bd9
maarxx <chris.lewark@gmail.com>
2017-03-25 14:39:14 -0400

    Opps 0.75F not 75

--- MoreAlerts/Alert_RestingBreakRisk.cs
+++ MoreAlerts/Alert_RestingBreakRisk.cs
@@ -19,7 +19,7 @@ namespace MoreAlerts
 
         protected override bool isPawnAffected(Pawn p)
         {
-            if (p.CurJob.def.reportString == "lying down." && !(p.needs.rest.GUIChangeArrow > 0) && p.needs.rest.CurLevel > 75)
+            if (p.CurJob.def.reportString == "lying down." && !(p.needs.rest.GUIChangeArrow > 0) && p.needs.rest.CurLevel > 0.75F)
             {
                 if (p.needs.mood.CurLevel < p.mindState.mentalBreaker.BreakThresholdMinor)
                 {
