4f8f204b4471c0a0022a378aae16e42bf60982e6
maarxx <chris.lewark@gmail.com>
2017-04-10 22:15:25 -0400

    Implements Issue #23

---
+++ MoreAlerts/Alert_Raider.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_Raider : Alert_Custom_Spawned
+    {
+
+        public Alert_Raider()
+        {
+            this.defaultPriority = AlertPriority.Critical;
+            this.defaultLabel = "raiders";
+            this.defaultExplanation = "There are raiders!";
+        }
+
+        protected override bool isPawnAffected(Pawn p)
+        {
+            if (p.RaceProps.Humanlike && p.Faction.HostileTo(Faction.OfPlayer))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
