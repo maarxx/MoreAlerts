eba3a224e2d809a65626d6e85ed07349bb70122f
maarxx <chris.lewark@gmail.com>
2017-05-01 23:37:28 -0400

    Implemented Issue #38

---
+++ MoreAlerts/Alert_Predator.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_Predator : Alert_Custom_Spawned
+    {
+
+        public Alert_Predator()
+        {
+            this.defaultPriority = AlertPriority.Medium;
+            this.defaultLabel = "predators";
+            this.defaultExplanation = "There are predators on your map.";
+        }
+
+        protected override bool isPawnAffected(Pawn p)
+        {
+            if (
+                    p.def.race.predator
+                )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
