e0b860ca7c0146d019d630cc94c2c7dba0f11001
Chris Lewark <chris.lewark@gmail.com>
2019-09-14 18:54:29 -0400

    Add Alert for Insects

---
+++ MoreAlerts/Alert_Insects.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_Insects : Alert_Custom_Spawned
+    {
+
+        public Alert_Insects()
+        {
+            this.defaultPriority = AlertPriority.Critical;
+            this.defaultLabel = "insects";
+            this.defaultExplanation = "There are insects!";
+        }
+
+        protected override bool isPawnAffected(Pawn p)
+        {
+            if (p.Faction != null && p.Faction.def == FactionDefOf.Insect && !p.Downed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
