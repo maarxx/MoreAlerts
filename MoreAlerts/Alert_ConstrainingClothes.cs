ef305734ecb0d9b0d58c2c727d46c0ff8f95fa6a
Chris Lewark <chris.lewark@gmail.com>
2019-09-26 08:17:31 -0400

    Add Constraining Clothes

---
+++ MoreAlerts/Alert_ConstrainingClothes.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_ConstrainingClothes : Alert_Custom_FreeColonistsSpawned
+    {
+
+        public Alert_ConstrainingClothes()
+        {
+            this.defaultLabel = "constraining clothes";
+            this.defaultExplanation = "Some nudists have constraining clothes.";
+        }
+
+        protected override bool isPawnAffected(Pawn p)
+        {
+            List<Thought> thoughts = new List<Thought>();
+            p.needs.mood.thoughts.GetAllMoodThoughts(thoughts);
+            foreach (Thought t in thoughts)
+            {
+                if (t.def.defName == "ClothedNudist")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
