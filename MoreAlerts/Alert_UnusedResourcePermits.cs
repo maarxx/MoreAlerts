e92e991f070f2abfed0cba9ac4db2486a219be89
maarxx <chris.lewark@gmail.com>
2020-11-08 00:36:09 -0500

    Alert for Wasting Resource Permit

---
+++ MoreAlerts/Alert_UnusedResourcePermits.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_UnusedResourcePermits : Alert_Custom_FreeColonistsSpawned
+    {
+
+        public Alert_UnusedResourcePermits()
+        {
+            this.defaultPriority = AlertPriority.Medium;
+            this.defaultLabel = "unused resource permits";
+            this.defaultExplanation = "Some colonists have unused resource drop permits.";
+        }
+
+        protected override bool isPawnAffected(Pawn p)
+        {
+            if (p.royalty.HasAidPermit)
+            {
+                foreach (FactionPermit permit in p.royalty.AllFactionPermits)
+                {
+                    if (!permit.OnCooldown)
+                    {
+                        if (permit.Permit.defName == "SteelDrop") { return true; }
+                        if (permit.Permit.defName == "FoodDrop") { return true; }
+                        if (permit.Permit.defName == "SilverDrop") { return true; }
+                        if (permit.Permit.defName == "GlitterMedDrop") { return true; }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
