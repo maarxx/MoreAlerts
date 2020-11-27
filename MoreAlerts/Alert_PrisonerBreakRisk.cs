4a5167797bf192dc5074bf0f377d0d76457863ed
maarxx <chris.lewark@gmail.com>
2020-11-27 01:15:38 -0500

    Alert for Prisoner Break Risk

---
+++ MoreAlerts/Alert_PrisonerBreakRisk.cs
@@ -0,0 +1,39 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_PrisonerBreakRisk : Alert_Custom_PrisonersOfColonySpawned
+    {
+
+        public Alert_PrisonerBreakRisk()
+        {
+            this.defaultPriority = AlertPriority.Critical;
+            this.defaultLabel = "prisoner break risk";
+            this.defaultExplanation = "Some prisoners are break risk.";
+        }
+
+        protected override bool isPawnAffected(Pawn p)
+        {
+            if (p.needs.mood.CurLevel < p.mindState.mentalBreaker.BreakThresholdMinor)
+            {
+                if (pawnCanMove(p))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool pawnCanMove(Pawn p)
+        {
+            return p.health.capacities.CanBeAwake
+                   && p.health.capacities.CapableOf(PawnCapacityDefOf.Moving)
+                   && !p.health.InPainShock;
+        }
+    }
+}
