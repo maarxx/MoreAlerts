730359b6ebb5e2632ffca8ac80d7ed47fd423e83
maarxx <chris.lewark@gmail.com>
2023-02-14 11:38:08 -0500

    Alert on Forbidden Empty Hydroponics

---
+++ MoreAlerts/Alert_ForbiddenEmptyHydroponics.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace MoreAlerts
+{
+    class Alert_ForbiddenEmptyHydroponics : Alert_Custom
+    {
+        public Alert_ForbiddenEmptyHydroponics()
+        {
+            this.defaultLabel = "idle hydroponics";
+            this.defaultExplanation = "Some hydroponics are empty and forbidden.";
+        }
+
+        protected override void GetAffectedThings()
+        {
+            int curTick = Find.TickManager.TicksGame;
+            if (lastTick + 10 > curTick)
+            {
+                return;
+            }
+            else
+            {
+                this.affectedThings = new List<Thing>();
+                foreach (Map map in Find.Maps)
+                {
+                    foreach (Building b in map.listerBuildings.AllBuildingsColonistOfDef(DefDatabase<ThingDef>.GetNamed("HydroponicsBasin")))
+                    {
+                        Building_PlantGrower bpg = (Building_PlantGrower)b;
+                        if (bpg.IsForbidden(Faction.OfPlayer) && !bpg.PlantsOnMe.Any())
+                        {
+                            this.affectedThings.Add(bpg);
+                        }
+                    }
+                }
+                lastTick = curTick;
+            }
+        }
+    }
+}
